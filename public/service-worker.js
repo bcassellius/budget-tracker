dd60e8eb915d1b7c52c5ce0b77e05e79316e0119
Bri <bhilliker@gmail.com>
2021-10-02 14:09:25 -0500

    troubleshooting backend

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,10 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     '/',
-    './public/index.html',
-    './public/css/styles.css',
-    './public/js/index.js',
-    './public/js/ibd.js'
+    '/index.html',
+    '/css/styles.css',
+    '/js/index.js',
+    '/js/ibd.js'
 ]
 
 self.addEventListener('install', function (e) {
